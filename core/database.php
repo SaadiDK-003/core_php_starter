ae49b51adc1157f769a6dc44c2c427cace123bc6
SaadiDK-003 <pcremote059@gmail.com>
2025-03-04 13:28:09 +0500

    change

--- core/database.php
+++ core/database.php
@@ -10,6 +10,23 @@ $db = mysqli_connect(env("HOST"), env("USER"), env("PWD"), env("DB"));
 $tailwind = env("TAILWIND");
 $alpineJs = env("ALPINE_JS");
 
+$userName = '';
+$userEmail = '';
+$userPwd = '';
+$userRole = '';
+$userStatus = '';
+
+if (isset($_SESSION['user'])) {
+      $userid = $_SESSION['user'];
+      $user_Q = $db->query("SELECT * FROM `users` WHERE `id`='$userid'");
+      $user = $user_Q->fetch_object();
+      $userName = $user->username;
+      $userEmail = $user->email;
+      $userPwd = $user->password;
+      $userRole = $user->role;
+      $userStatus = $user->status;
+}
+
 require_once 'functions.php';
 
 
--- includes/header.php
+++ includes/header.php
@@ -1,8 +1,16 @@
 <header class="d-flex align-items-center">
       <div class="container d-flex align-items-center justify-content-between">
-            <h3 class="text-white"><?= env("TITLE") ?></h3>
+            <a href="./" class="text-decoration-none">
+                  <h3 class="text-white"><?= env("TITLE") ?></h3>
+            </a>
             <div class="buttons-wrapper">
-                  <a href="./logout.php" class="btn btn-danger">LOGOUT</a>
+                  <?php if (isLoggedIn()): ?>
+                        <?php if ($userRole == ""): ?>
+                        <?php endif; ?>
+                        <a href="./logout.php" class="btn btn-danger">LOGOUT</a>
+                  <?php else: ?>
+                        <a href="./login.php" class="btn btn-success">LOGIN</a>
+                  <?php endif; ?>
             </div>
       </div>
 </header>
