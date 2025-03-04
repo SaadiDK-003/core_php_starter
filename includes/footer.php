a2e783d5f412057b00557ca935918b2750c2c6ac
SaadiDK-003 <pcremote059@gmail.com>
2025-03-04 14:11:36 +0500

    change in footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,9 @@
-<footer>
-      <div class="container"></div>
+<footer class="d-flex align-items-center justify-content-center">
+      <div class="container">
+            <div class="row">
+                  <div class="col-12">
+                        <h4 class="text-center text-white mb-0"><?= date('Y') ?> All Rights Reserved.</h4>
+                  </div>
+            </div>
+      </div>
 </footer>
